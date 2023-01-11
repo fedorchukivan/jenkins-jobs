63b078399425dc0eaaed19660465583248c03cc4
Ivan <fedorchuk.kp93@gmail.com>
2023-01-11 19:58:37 +0200

    Added job

---
+++ jobs/pipeline.groovy
@@ -0,0 +1,36 @@
+pipelineJob('app') {
+  displayName('app')
+  logRotator {
+    numToKeep(5)
+  }
+    parameters{
+      gitParameter{
+        name('GIT_BRANCH')
+        defaultValue('master')
+        description('Branch or tag to use for jobs')
+        type('PT_BRANCH_TAG')
+        branch('')
+        branchFilter('origin/(.)')
+        tagFilter('')
+        sortMode('DESCENDING_SMART')
+        selectedValue('NONE')
+        useRepository('')
+        quickFilterEnabled(true)
+      }
+      booleanParam('FAIL_ON_XRAY', true, 'Fail the build if xray scan returns violations')
+    }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            github('fedorchukivan/devops-app')
+          }
+          branches('${GIT_BRANCH}')
+        }
+      }
+      scriptPath('Jenkinsfile')
+      lightweight(false)
+    }
+  }
+}
